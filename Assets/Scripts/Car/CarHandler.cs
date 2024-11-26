9f6d4c9cc196d0babdbb455b903cc0e17b1b35b7
MICHAL-PC\Admin <brozda.it@gmail.com>
2024-11-26 22:10:35 +0100

    added chase camera and improved car handling

--- Assets/Scripts/Car/CarHandler.cs
+++ Assets/Scripts/Car/CarHandler.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class CarHandler : MonoBehaviour
@@ -8,9 +7,17 @@ public class CarHandler : MonoBehaviour
     [SerializeField]
     Rigidbody rb;
 
+    [SerializeField]
+    Transform gameModel;
+    //Max values
+    float maxForwardVelocity = 30;
+    float maxSteerVelocity = 2;
+
+    //Multipliers
     float accelerationMultiplier = 3;
     float breakaMultiplier = 15;
     float steeringMultiplier = 5;
+
 
     Vector2 input = Vector2.zero;
     void Start()
@@ -20,7 +27,7 @@ public class CarHandler : MonoBehaviour
 
     void Update()
     {
-        
+        gameModel.transform.rotation = Quaternion.Euler(0, rb.velocity.x * 5, 0);
     }
 
     private void FixedUpdate()
@@ -34,12 +41,18 @@ public class CarHandler : MonoBehaviour
             Break();
 
             Steer();
+
+        if(rb.velocity.z <= 0)
+            rb.velocity = Vector3.zero;
     }
 
     void Accelerate()
     {
         rb.drag = 0;
-        rb.AddForce(transform.forward * accelerationMultiplier * input.y);  
+        if(rb.velocity.z >= maxForwardVelocity)
+            return;
+
+        rb.AddForce(rb.transform.forward * accelerationMultiplier * input.y);  
     }
 
     void Break()
@@ -53,7 +66,19 @@ public class CarHandler : MonoBehaviour
     {
      if (Mathf.Abs(input.x) > 0)
         {
-            rb.AddForce(rb.transform.right * steeringMultiplier * input.x);
+            float speedBasedSteerLimit = rb.velocity.z / 5.0f;
+            speedBasedSteerLimit = Mathf.Clamp01(speedBasedSteerLimit);
+            rb.AddForce(rb.transform.right * steeringMultiplier * input.x * speedBasedSteerLimit);
+
+            float normalizedX = rb.velocity.x / maxSteerVelocity;
+
+            normalizedX = Mathf.Clamp(normalizedX, -1.0f, 1.0f);
+
+            rb.velocity = new Vector3(normalizedX * maxSteerVelocity, 0, rb.velocity.z);
+        }
+        else
+        {
+            rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(0, 0, rb.velocity.z), Time.fixedDeltaTime * 3);
         }
     }
 
